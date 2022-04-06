2fe22428773465afabe9a1ef4156a57f39102e76
Carlos Aceituno <caceituno-2017478@kinal.edu.gt>
2022-04-05 21:34:44 -0600

    registrar

--- src/app/components/registro/registro.component.ts
+++ src/app/components/registro/registro.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { usuarios } from 'src/app/models/empresa.model';
+import { empresaService } from 'src/app/services/empresa.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.scss']
+  styleUrls: ['./registro.component.scss'],
+  providers: [empresaService]
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  public empresaModelPost: usuarios;
+
+  constructor(private _empresaService: empresaService, private _router: Router) {
+      this.empresaModelPost = new usuarios('','','','','','');
+  }
 
   ngOnInit(): void {
   }
 
+  postEmpresa(){
+    this._empresaService.agregarEmpresa(this.empresaModelPost).subscribe(
+      (response)=>{
+        console.log(response)
+        this._router.navigate(["login"])
+      },
+      (error)=>{
+        console.log(<any>error);
+      }
+    )
+  }
 }
